ef9384209aee0657e85a50b3d8dde1aab2fb0cc3
zhangxing <1017073490@qq.com>
2020-11-27 21:12:51 +0800

    项目初始化

---
+++ guli-ware/src/test/java/com/zhangxing/guli/ware/GuliWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangxing.guli.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
